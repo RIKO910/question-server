338d556671bed243cba37be80e9747a7fea3a66a
RIKO910 <tarikulislamriko910@gmail.com>
2022-11-08 19:32:49 +0600

    final

--- index.js
+++ index.js
@@ -14,26 +14,173 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("leve1term1");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level1term1', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level1term1', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level1term2");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level1term2', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level1term2', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level2term1");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level2term1', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level2term1', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
 
-        await client.connect();
-        const usersCollection = client.db("express").collection("leve1term1");
-        // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
-        // const result =await usersCollection.insertOne(user);
-        // console.log(`User inserted with id: ${result.insertedId}`)
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level2term2");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level2term2', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level2term2', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
 
-        app.get('/level1term1',async(req,res)=>{  
-            const query ={};
-            const cursor =usersCollection.find(query);
-            const users =await cursor.toArray();
-            res.send(users);
-        });
-        //add a new user
-        app.post('/level1term1', async(req, res) => {
-            const newUser = req.body;
-            console.log('adding new user', newUser);
-            const result = await usersCollection.insertOne(newUser);
-            res.send(result)
-        });
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level3term1");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level3term1', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level3term1', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level3term2");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level3term2', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level3term2', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level4term1");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level4term1', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level4term1', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+
+        {
+            await client.connect();
+            const usersCollection = client.db("express").collection("level4term2");
+            // const user ={name:'mahiya mahi ',email:'mahiya@gmail.com'}
+            // const result =await usersCollection.insertOne(user);
+            // console.log(`User inserted with id: ${result.insertedId}`)
+            app.get('/level4term2', async (req, res) => {
+                const query = {};
+                const cursor = usersCollection.find(query);
+                const users = await cursor.toArray();
+                res.send(users);
+            });
+            //add a new user
+            app.post('/level4term2', async (req, res) => {
+                const newUser = req.body;
+                console.log('adding new user', newUser);
+                const result = await usersCollection.insertOne(newUser);
+                res.send(result)
+            });
+        }
+
+
     }
 
     finally {
